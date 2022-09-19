347de5e353645cbf3c7e9cd55ea6d465e39195a3
Baptiste Simon <baptistesimon@MacBook-Air-de-Baptiste.local>
2022-09-19 15:51:56 +0200

    Modification de Event.php et création de show_event.phtml

--- mini_projet/framework/App/Models/Event.php
+++ mini_projet/framework/App/Models/Event.php
@@ -16,19 +16,16 @@ class Event extends AbstractModel{
             INNER JOIN Categories c ON e.category_id = c.id'
         );
     }
-<<<<<<< HEAD
-}
-=======
     
     public function find(int $id): ?array
     {
         return $this->db->getOne(
-            'SELECT e.title, e.description, e.pictures, e.created_at, e.started_at
+            'SELECT e.title, e.description, e.pictures, e.created_at, e.started_at, u.username, c.name
             FROM Events e
-            INNER JOIN users u ON e.user_id = u.id
-            INNER JOIN categories c ON e.category_id = c.id
+            INNER JOIN Users u ON e.user_id = u.id
+            INNER JOIN Categories c ON e.category_id = c.id
             WHERE Events.id = ?', [$id]
         );
     }
 }
->>>>>>> b4df8f5b6b5b839d0a47756da7c8f988942fc0f3
+
